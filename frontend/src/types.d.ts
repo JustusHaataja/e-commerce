1ddd85099d2a720db21a636f4459a939022e5327
Justus Haataja <justus.haataja@hotmail.com>
2026-05-06 14:25:45 +0300

    chore: update dependencies and improve TypeScript configuration
    - Updated React and related packages to the latest versions.
    - Adjusted FontAwesome packages to the latest versions.
    - Enhanced TypeScript configuration with new compiler options.
    - Added support for additional image formats in type declarations.
    - Updated package-lock.json to reflect changes in dependencies.

--- frontend/src/types.d.ts
+++ frontend/src/types.d.ts
@@ -1,9 +1,14 @@
+/// <reference types="vite/client" />
+
 declare module '*.css';
 declare module '*.scss';
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
 declare module '*.svg';
+declare module '*.avif';
+declare module '*.gif';
+declare module '*.webp';
 
 declare module 'swiper/css';
 declare module 'swiper/css/navigation';
